3a7c1c93a11a20ed325eac40255199a8c5cf68fd
Almaz <minnexanov.a@mail.ru>
2023-01-27 19:12:53 +0300

    task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,5 +3,25 @@
 // 5 -> Пятница
 
 Console.Clear();
-Console.WriteLine("Введите день недели: ");
-int number1 = int.Parse(Console.ReadLine())
+Console.WriteLine("Введите порядковый номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+if(number == 1)
+{
+    Console.Write("понедельник");
+}
+if(number == 2)
+{
+    Console.Write("вторник");
+}
+if(number == 3)
+{
+    Console.Write("среда");
+}
+if(number == 4)
+{
+    Console.Write("четверг");
+}
+if(number == 5)
+{
+    Console.Write("пятница");
+}
